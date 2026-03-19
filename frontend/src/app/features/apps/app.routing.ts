83b0942cb9f5ab54f4fa04f340eeb668a94d6b44
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-03-19 00:42:39 -0300

    feat: create terms of use of daily habits

--- frontend/src/app/features/apps/app.routing.ts
+++ frontend/src/app/features/apps/app.routing.ts
@@ -6,4 +6,9 @@ export const appRoutes: Routes = [
     loadComponent: () =>
       import('./daily-habits/privacy-policy/privacy-policy').then((m) => m.PrivacyPolicy),
   },
+  {
+    path: 'app/daily-habits/terms-of-use',
+    loadComponent: () =>
+      import('./daily-habits/terms-of-use/terms-of-use').then((m) => m.TermsOfUse),
+  },
 ];
