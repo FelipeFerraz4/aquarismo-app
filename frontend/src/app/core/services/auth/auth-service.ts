5dd096ed569c5e3e16e2ff22e5e0aefdc403576d
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-05-05 02:35:53 -0300

    chore: added keycloak dependency

--- frontend/src/app/core/services/auth/auth-service.ts
+++ frontend/src/app/core/services/auth/auth-service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import Keycloak from 'keycloak-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
+  // public kc: Keycloak = new Keycloak({
+  //   url: 'http://localhost:8080',
+  //   realm: 'BlueFox',
+  //   clientId: 'aquarismo-web',
+  // });
+
+  // public init(): Promise<any> {
+  //   return new Promise((resolve, reject) => {
+  //     this.kc.init({
+  //       onLoad: 'login-required',
+  //       checkLoginIframe: false,
+  //       // Configuraões necessárias para redirecionamento
+  //       redirectUri: `${window.location.origin}/`,
+  //       silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+  //     })
+  //     .then(() => resolve())
+  //     .catch(reject);
+  //   });
+  // }
 
 }
