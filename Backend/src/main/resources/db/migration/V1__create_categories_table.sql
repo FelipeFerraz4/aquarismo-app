2d974d8a01887ecc7ff089e81f028281b9a44ad6
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-05-29 17:36:45 -0300

    feat: create categories migration

---
+++ Backend/src/main/resources/db/migration/V1__create_categories_table.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE categories (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    slug VARCHAR(255) NOT NULL UNIQUE,
+    status VARCHAR(50) NOT NULL
+        CHECK (
+            status IN (
+                       'ACTIVE',
+                       'INACTIVE',
+                       'SUSPENDED',
+                       'PENDING',
+                       'DELETED'
+            )
+        ),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
