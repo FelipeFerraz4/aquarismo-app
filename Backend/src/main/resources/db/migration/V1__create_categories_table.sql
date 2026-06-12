a75c4e9de81759a53883231b0a8d18935e80beee
FelipeFerraz4 <felipeferraz2402@gmail.com>
2026-06-12 20:30:31 -0300

    feat: improving the quality of migration

--- Backend/src/main/resources/db/migration/V1__create_categories_table.sql
+++ Backend/src/main/resources/db/migration/V1__create_categories_table.sql
@@ -1,20 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE categories (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    slug VARCHAR(255) NOT NULL UNIQUE,
-    status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE'
-        CHECK (
-            status IN (
-                       'ACTIVE',
-                       'INACTIVE',
-                       'SUSPENDED',
-                       'PENDING',
-                       'DELETED'
-            )
-        ),
+    slug VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE',
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    CONSTRAINT pk_categories PRIMARY KEY (id),
+    CONSTRAINT uq_categories_slug UNIQUE (slug),
+    CONSTRAINT chk_categories_status CHECK (
+        status IN ('ACTIVE', 'INACTIVE', 'SUSPENDED', 'PENDING', 'DELETED')
+    )
 );
--- Backend/src/main/resources/db/migration/V2__create_table_authors.sql
+++ Backend/src/main/resources/db/migration/V2__create_table_authors.sql
@@ -1,15 +1,18 @@
 CREATE TABLE authors (
-     id UUID NOT NULL,
+     id UUID NOT NULL DEFAULT gen_random_uuid(),
      name VARCHAR(255) NOT NULL,
      bio TEXT,
      profile_picture_url VARCHAR(255),
      slug VARCHAR(255) NOT NULL,
      email VARCHAR(255) NOT NULL,
-     status VARCHAR(50) NOT NULL,
+     status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE',
      created_at TIMESTAMP WITH TIME ZONE NOT NULL,
      updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
      CONSTRAINT pk_authors PRIMARY KEY (id),
      CONSTRAINT uq_authors_slug UNIQUE (slug),
-     CONSTRAINT uq_authors_email UNIQUE (email)
+     CONSTRAINT uq_authors_email UNIQUE (email),
+     CONSTRAINT chk_authors_status CHECK (
+         status IN ('ACTIVE', 'INACTIVE', 'SUSPENDED', 'PENDING', 'DELETED')
+     )
 );
